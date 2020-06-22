991a5b32ee35bf3aa4866003d5bc3f3d8aa0ca3a
Luis Damiano <damiano.luis@gmail.com>
2020-06-22 09:05:16 -0500

    Add memoization to some time intensive functions

--- R/zzz.R
+++ R/zzz.R
@@ -10,5 +10,27 @@
 }
 
 .onLoad <- function(libname, pkgname) {
+  useCache <- tryCatch(
+    expr  = { !is.null(asNamespace("memoise")) },
+    error = function(e) { FALSE }
+  )
 
+  if (useCache) {
+    # Default cache filesystem
+    cache <- function(f) {
+      memoise::memoise(
+        f     = f,
+        cache = memoise::cache_filesystem("./.cache")
+      )
+    }
+
+    # Memoise these functions only
+    # Example from https://github.com/r-lib/memoise/blob/master/R/memoise.R
+    make_vehicle_polygons <<- cache(make_vehicle_polygons)
+    reshape_polygons      <<- cache(reshape_polygons)
+    make_grid             <<- cache(make_grid)
+    chop_polygons         <<- cache(chop_polygons)
+    aggregate_polygons    <<- cache(aggregate_polygons)
+    smooth_polygons       <<- cache(smooth_polygons)
+  }
 }
